ae0e12f3228c1acb48be853bf1d312691617b73c
tobias innleggen <tin017@uib.no>
2022-05-28 18:06:11 +0200

    Tried to make point counter work but it hase some bugs

--- src/Components/questionsPage.js
+++ src/Components/questionsPage.js
@@ -4,8 +4,21 @@ import { questionsDatabase } from './questionsDatabase'
 //Next step should be that the counter decrease each time you uncheck a checkbox and increase if it is checked again
 //Then add the points of questions not just one point each time
 export default function Questions({Data}){
-    const [count, setCount] = useState(0);
-    return(
+    const [count, setCount] = useState(0)
+    //All the checkboxes uses this state so that creates some bugs
+    const [checked, setChecked] = useState(false)
+    // const HandleCheck = ({Data}) =>{
+    //     // const [checked, setChecked] = useState(false)
+    //     if(!checked){
+    //         setCount(count + Data)
+    //         } else if(checked){
+    //             setCount(count- Data)
+    //         }   
+    //         setChecked(!checked)
+    //         console.log(checked)
+    // }
+
+    return(        
         <div className='DareNight'>
             <h1 className='DareNightHeader'>Dare Night</h1>
             <h2 className='scoreCounter'>{count}</h2>
@@ -17,7 +30,15 @@ export default function Questions({Data}){
                         <h3 className="questionDificulty">{value.dificulty}</h3>
                         <h3 className="mainQuestion">{value.question_text}</h3>
                         <h3 className="points">{value.points}</h3>
-                        <input onChange={()=>setCount(count +1)} className= "checkbox" type='checkbox' ></input>
+                        <input onChange={()=>{
+                            if(!checked){
+                            setCount(count + value.points)
+                            } else if(checked){
+                                setCount(count-value.points)
+                            }
+                            setChecked(!checked)
+                            console.log(checked)
+                        }} className= "checkbox" type='checkbox'></input>
                     </div>
                 </div>
             ))
@@ -25,4 +46,4 @@ export default function Questions({Data}){
             </div>
         </div>
     )
-}
+}   
