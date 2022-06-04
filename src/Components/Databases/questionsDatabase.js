3f9142ca78166a13686406515d8ec691a35fd6fc
tobias innleggen <tin017@uib.no>
2022-06-04 16:05:50 +0200

    Added all questions

--- src/Components/Databases/questionsDatabase.js
+++ src/Components/Databases/questionsDatabase.js
@@ -108,12 +108,225 @@ export const questionsDatabase = [
     points: 1
     },
     {
-    question_id:10,
+    question_id:19,
     dificulty: 'easy',
     question_text: 'Finn Ludvig Holberg',
     points: 1
     },
-
-
-
+    {
+    question_id:20,
+    dificulty: 'medium',
+    question_text: 'Sing like there is nobody listening',
+    points: 2
+    },
+    {
+    question_id:21,
+    dificulty: 'medium',
+    question_text: 'Dance like nobodys around',
+    points: 2
+    },
+    {
+    question_id:22,
+    dificulty: 'medium',
+    question_text: 'Fri til en på laget få det til å virke som et reelt frieri. Resten av gruppa må hype opp seansen',
+    points: 3
+    },
+    {
+    question_id:23,
+    dificulty: 'medium',
+    question_text: 'Lat som du urinerer offentlig',
+    points: 2
+    },
+    {
+    question_id:24,
+    dificulty: 'medium',
+    question_text: 'Hopp i havet (3 poeng). Hvis du hopper med alle klær på (+2 poeng). Hvis du bader naken (+4 poeng).',
+    points: 3
+    },
+    {
+    question_id:25,
+    dificulty: 'medium',
+    question_text: 'Spis en chilli',
+    points: 3
+    },
+    {
+    question_id:26,
+    dificulty: 'medium',
+    question_text: 'Krabb opp trappen til Johanneskirken',
+    points: 3
+    },
+    {
+    question_id:27,
+    dificulty: 'medium',
+    question_text: 'Gå til kassen med en pakke kondomer og ikke nok penger. Legg kondomene tilbake når du “innser” at du ikke har råd. Gå så tilbake til den samme kassen og betal for gummistrikk og plastfolie.',
+    points: 3
+    },
+    {
+    question_id:28,
+    dificulty: 'medium',
+    question_text: 'Utford en fremmed til chuggekonkeranse',
+    points: 3
+    },
+    {
+    question_id:29,
+    dificulty: 'medium',
+    question_text: 'Forklar poenget med overgangsfelt til en som går på rødt lys',
+    points: 3
+    },
+    {
+    question_id:30,
+    dificulty: 'medium',
+    question_text: 'Bruk lengst mulig tid over over et overgangsfelt',
+    points: 3
+    },
+    {
+    question_id:31,
+    dificulty: 'medium',
+    question_text: 'Gå med undertøyet på utsiden av klærne i minst 15 min',
+    points: 3
+    },
+    {
+    question_id:32,
+    dificulty: 'medium',
+    question_text: 'Start en chant',
+    points: 3
+    },
+    {
+    question_id:33,
+    dificulty: 'medium',
+    question_text: 'Ta tak i hånden til en fremmed, og lat som om dere leier. Spør så “hva skal vi finne på”.',
+    points: 3
+    },
+    {
+    question_id:34,
+    dificulty: 'medium',
+    question_text: 'Prøv å få med noen på allsang',
+    points: 3
+    },
+    {
+    question_id:35,
+    dificulty: 'medium',
+    question_text: 'Bestill noe fra en kiosk uten å lukke munnen',
+    points: 3
+    },
+    {
+    question_id:36,
+    dificulty: 'medium',
+    question_text: 'Få det til å se ut som du har tisset på deg',
+    points: 3
+    },
+    {
+    question_id:37,
+    dificulty: 'Hard',
+    question_text: 'Sleik undersiden av skoen til en annen',
+    points: 5
+    },
+    {
+    question_id:38,
+    dificulty: 'Hard',
+    question_text: 'Prøv å overbevis en fremmed om at dere kjenner hverandre fra før',
+    points: 4
+    },
+    {
+    question_id:39,
+    dificulty: 'Hard',
+    question_text: 'Utfordre en fremmed til dance battle',
+    points: 4
+    },
+    {
+    question_id:40,
+    dificulty: 'Hard',
+    question_text: 'Lag en scene på McDonalds/Burger king eller liknende',
+    points: 5
+    },
+    {
+    question_id:41,
+    dificulty: 'Hard',
+    question_text: 'Be en fremmed om å filme deg. Når vedkommende starter å filme så skjeller du de ut for at de filmer deg uten lov',
+    points: 5
+    },
+    {
+    question_id:42,
+    dificulty: 'Hard',
+    question_text: 'Fremfør et nummer på torgalmeningen',
+    points: 5
+    },
+    {
+    question_id:43,
+    dificulty: 'Hard',
+    question_text: 'Trauma-dump en fremmed',
+    points: 4
+    },
+    {
+    question_id:44,
+    dificulty: 'Hard',
+    question_text: 'Flørt med en fremmed og spør om nummeret deres',
+    points: 4
+    },
+    {
+    question_id:45,
+    dificulty: 'Hard',
+    question_text: 'French kiss en på laget',
+    points: 5
+    },
+    {
+    question_id:46,
+    dificulty: 'Hard',
+    question_text: 'Be om hjelp til å finne en pakke kondomer uten å snakke',
+    points: 5
+    },
+    {
+    question_id:47,
+    dificulty: 'Hard',
+    question_text: 'Kyss en fremmed(husk Consent)',
+    points: 5
+    },
+    {
+    question_id:49,
+    dificulty: 'Hard',
+    question_text: 'Leg til alle eksene dine i en familiechat',
+    points: 5
+    },
+    {
+    question_id:50,
+    dificulty: 'Hard',
+    question_text: 'Legg alle du har lugget med i en gruppechat og spør gjett hva dere har til felles',
+    points: 5
+    },
+    {
+    question_id:51,
+    dificulty: 'Hard',
+    question_text: 'Overtal en fremmed til å utføre en av utfordringene på listen.',
+    points: 5
+    },
+    {
+    question_id:52,
+    dificulty: 'Hard',
+    question_text: 'Gå inn på et offentlig toalett og bank på en opptatt bås og spør om det er plass til en til',
+    points: 4
+    }, 
+    {
+    question_id:53,
+    dificulty: 'Hard',
+    question_text: 'Vipps 50 kr til 41383975',
+    points: 5
+    },
+    {
+    question_id:54,
+    dificulty: 'Hard',
+    question_text: 'Start en samtale med en fremmed der du tilsynelatende snakker norsk, men egentlig bare er tullespråk',
+    points: 4
+    },
+    {
+    question_id:55,
+    dificulty: 'Hard',
+    question_text: 'Gå inn på en nattklubb og gjør matte',
+    points: 4
+    },
+    {
+    question_id:51,
+    dificulty: 'Hard',
+    question_text: 'Gå med gul vest inn på bybanen og lat som du har biltettkontroll',
+    points: 5
+    }
 ]
